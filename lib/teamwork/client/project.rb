efd8cef723d040e6ef460a4a26adc3baa7f5bca2
Carmin Moutinho <echo>
2017-10-18 14:30:01 -0400

    fixes to get task lists

--- lib/teamwork/client/project.rb
+++ lib/teamwork/client/project.rb
@@ -82,7 +82,7 @@ module Teamwork
       # GET a projects task lists
       # Return Teamwork::Thing
       def project_task_lists(id)
-        object_from_response(:get, "projects/#{id}/tasklists", "project")
+        objects_from_response(:get, "projects/#{id}/tasklists", "tasklists")
       end
 
       # PUT a start to a project
